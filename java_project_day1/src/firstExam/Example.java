1b77bf729214849de3d4c65b3634fca7263e1ce2
user1 <user1@DESKTOP-1MR1E4B>
2024-09-27 17:00:41 +0900

    Change-Id: I0000000000000000000000000000000000000000

---
+++ java_project_day1/src/firstExam/Example.java
@@ -0,0 +1,25 @@
+package firstExam;
+
+public class Example { //클래스 이름의 선언
+	//들여쓰기에 주의하여 작성
+	public static void main(String[] args) {
+		System.out.println("이름 : 김서건");  //print"ln"은 엔터의 기능(줄 바꾸기)
+		System.out.println("주소 : 서울시 관악구 신림동"); 
+		System.out.print("나의 각오 : 세상에서 돈이 전부는 아니지만,");
+		System.out.println(" 연봉은 곧 능력을 나타내기 때문에");
+		System.out.println("          고연봉 개발자가 되겠습니다!!");
+		
+		 //카멜표기법 +스네이크표기법 (단어연결시 "_" 사용)
+
+/* int num1 = 1; // 초기에 값을 선언 = 초기화
+int num2 = num1 + 1;
+		
+		
+double PI; 
+	PI = 3.14; //변하지 않는 수 -모든글자가 대문자
+		System.out.println(num1);	
+		System.out.println(num2);
+		System.out.println(PI); */
+
+	}		
+}
